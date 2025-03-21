1c1bc0749e5c16d4b7c2a03fa99d4e79ad679ed4
Rohan271004 <rp285518@gmail.com>
2025-03-21 16:08:31 +0530

    Third Commit

--- electronics/models.py
+++ electronics/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
+    image = models.ImageField(upload_to = 'images/', null=True , blank=True)
 
     def __str__(self):
         return self.name
